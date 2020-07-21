2720f4584a57a262fcf6065ecbc25e5584827533
Daniel Hogan <hogan.daniel@hotmail.com>
2020-07-21 14:19:48 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import github from "./assets/github.png";
 
 function App() {
+  const [username, setUsername] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Valorant Stat Tracker</h1>
       </header>
+      <main>
+        <form>
+          <label htmlfor="username">Enter your VALORANT username:</label>
+          <input
+            disabled
+            placeholder="Coming soon..."
+            onChange={(e) => setUsername(setUsername(e.target.value))}
+            name="username"
+            value={username}
+            className="username"
+          />
+          <button type="submit">Track Stats</button>
+        </form>
+      </main>
+      <footer>
+        <a href="">
+          <img src={github} />
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
