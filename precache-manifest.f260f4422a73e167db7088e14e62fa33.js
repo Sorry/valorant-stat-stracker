f44d6e7b61f106e17be58af0eafa7ef5d25893e3
Daniel Hogan <hogan.daniel@hotmail.com>
2020-07-21 14:37:12 -0700

    Updates

---
+++ precache-manifest.f260f4422a73e167db7088e14e62fa33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58df23830baf7521c285a238a461a92f",
+    "url": "/valorant-stat-stracker/index.html"
+  },
+  {
+    "revision": "3251825786a1db95e8f3",
+    "url": "/valorant-stat-stracker/static/css/main.76099070.chunk.css"
+  },
+  {
+    "revision": "6a51f83fe1f9fea145f7",
+    "url": "/valorant-stat-stracker/static/js/2.f6657430.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/valorant-stat-stracker/static/js/2.f6657430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3251825786a1db95e8f3",
+    "url": "/valorant-stat-stracker/static/js/main.6fda6984.chunk.js"
+  },
+  {
+    "revision": "46d875f72c3096837f91",
+    "url": "/valorant-stat-stracker/static/js/runtime-main.d4b77290.js"
+  },
+  {
+    "revision": "ff1d322daa20d811d983e49ee7a0a4a3",
+    "url": "/valorant-stat-stracker/static/media/github.ff1d322d.png"
+  },
+  {
+    "revision": "82f24771a312ef399d3a9165e514df92",
+    "url": "/valorant-stat-stracker/static/media/k5c2c4ndk0s41.82f24771.png"
+  }
+]);
